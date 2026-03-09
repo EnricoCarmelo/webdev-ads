87ccaec1be02cdf46ec932fbd6572bbec927b6e8
Prof PC Lab 32 <prof@unilago.edu.br>
2026-03-09 20:04:33 -0300

    aula si 09-03

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const { engine } = require("express-handlebars")
 const bodyParser = require('body-parser')
+const path = require("path")
 
 const app = express()
 
@@ -9,6 +10,9 @@ app.use(bodyParser.json())
 
 app.engine('handlebars', engine({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
+
+app.use(express.static(path.join(__dirname, 'views/css/'))) // Indicando pasta de arquivos estáticos css, pode ser usado para js e outros.
+app.use(express.static(path.join(__dirname, 'views/js/')))
 
 app.get("/", (req, res) => {
     res.render('index')
@@ -22,5 +26,5 @@ app.get("/sobre/{:id}", (req, res) => {
 })
 
 app.listen(3000, () => {
-    console.log('Servidor rodando em http://localhost:3000')
+    console.log('Servidor rodando http://localhost:3000')
 })
